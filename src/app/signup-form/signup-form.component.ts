fcf8696007bdc44cd8b29987d49a36dee3946793
mohamedkaabi <mohamed.kaabi.cap@gmail.com>
2021-10-28 20:48:46 +0100

    formGroups inside formGroup which contains multiple fromControllers

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -8,14 +8,17 @@ import { FormGroup, FormControl, Validators,  } from '@angular/forms';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', Validators.required,UsernameValidators.shouldBeUnique),
-    password: new FormControl('', Validators.required)
+    account: new FormGroup ({
+        username: new FormControl('', Validators.required,UsernameValidators.shouldBeUnique),
+        password: new FormControl('', Validators.required)
+    })
+    
   });
 
   get username(){
-    return this.form.get('username')
+    return this.form.get('account.username')
   }
   get password(){
-    return this.form.get('password')
+    return this.form.get('account.password')
   }
 }
