3bdc13d36b2d03eed58c64f3eca496a71def6f66
mohamedkaabi <mohamed.kaabi.cap@gmail.com>
2021-11-03 01:17:02 +0100

    retry method

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -10,64 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit  {
-
-    posts:any = [];
-
+  
+  posts:any = [];
+  
   constructor(public service: PostService) {}
-
+  
 
   ngOnInit()
   {
-          this.service.getAll()
-                .subscribe( posts => this.posts = posts);     
+    this.service.getAll()
+    .subscribe( posts => this.posts = posts);     
   }
 
   createPost(input: HTMLInputElement)
   {
-          let post: any = {title: input.value};
-          this.posts.splice(0,0,post);
-
-          input.value = ''
-          
-            this.service.create(post)
-                .subscribe(
-                  newPost =>{post.id = newPost.id;},
-                          
-                  (error: appError) => {
-                          this.posts.splice(0,1);
-
-
-                              if ( error instanceof BadInput ) 
-                              {
-                            // this.form.setErrors(error.originalError); 
-                              }
-                              else 
-
-                              throw error;
-                            })
+    let post: any = {title: input.value};
+    this.posts.splice(0,0,post);
     
+    input.value = ''
+    
+    this.service.create(post)
+    .subscribe(
+      newPost =>{post.id = newPost.id;},
+      
+      (error: appError) => {
+        this.posts.splice(0,1);
+        
+        
+        if ( error instanceof BadInput ) 
+        {
+          // this.form.setErrors(error.originalError); 
+        }
+        else 
+        
+        throw error;
+      })
+        
   }
   updatePost(post: any)
   {
-          this.service.update(post)
-                .subscribe((updatedPost : any)=>
-                      {
-                         console.log(updatedPost )
-                      })
+    this.service.update(post)
+    .subscribe((updatedPost : any)=>
+    {
+      console.log(updatedPost )
+    })
   }
   deletePost(post: any) 
   {
-    let index = this.posts.indexOf(post);
-    this.posts.splice(index, 1); 
-      
-    this.service.delete(post.id)
-                .subscribe(
-                  null, 
-                  (error: appError) => {
-                    this.posts.splice(index, 0,post);
-                            if ( error instanceof notFoundError ) 
-                                  alert('This Post Has Already Been Deleted')
-                            else throw error;
-                      })
+    this.service.delete(post.id);;
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,8 @@
 import { BadInput } from './../common/validators/bad-input';
 import { appError } from './../common/validators/app-error';
 import { HttpClient } from '@angular/common/http';
-import {Observable, observable, throwError} from 'rxjs';
-import {catchError, map} from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { catchError, map, retry} from 'rxjs/operators';
 import { notFoundError } from '../common/validators/not-found-error';
 
 
@@ -16,47 +16,49 @@ export class DataService {
     getAll()
     {
       return this.http.get(this.url)
-                            .pipe (
-                             map((response :any) => JSON.parse(JSON.stringify(response)),
-                             catchError(this.handleError)
-                                  ));
+      .pipe (
+        map((response :any) => JSON.parse(JSON.stringify(response)),
+        catchError(this.handleError)
+        ));
     }
 
     create(resource: any)
     {
-    return this.http.post(this.url,
-                            JSON.parse(JSON.stringify(resource)))
-                            .pipe(
-                             map((response :any) => JSON.parse(JSON.stringify(response)),
-                             catchError(this.handleError)));
+      return this.http.post(this.url,
+        JSON.parse(JSON.stringify(resource)))
+        .pipe(
+          map((response :any) => JSON.parse(JSON.stringify(response)),
+          catchError(this.handleError)));
     }
     update(resource: any)
     {
       return this.http.patch(this.url + '/' + resource.id,
-                              JSON.parse(JSON.stringify({isRead: true}))) 
-                              .pipe(
-                               map((response :any) => JSON.parse(JSON.stringify(response)),
-                               catchError(this.handleError)));
+      JSON.parse(JSON.stringify({isRead: true}))) 
+      .pipe(
+        map((response :any) => JSON.parse(JSON.stringify(response)),
+        catchError(this.handleError)));
                            
     }
    
    
     delete(id : any){
 
-      return throwError(new appError());
+      // return throwError(new appError());
 
-        // return this.http.delete(this.url + '/' + id)
-        //                         .pipe(
-        //                          map((response :any) => JSON.parse(JSON.stringify(response)),
-        //                          catchError(this.handleError)));
+        return this.http.delete(this.url + '/' + id)
+        .pipe(
+          map((response :any) => JSON.parse(JSON.stringify(response)),
+          catchError(this.handleError),
+          ), retry(3) ) //as the new documentation position where it should the retry method be.
+          // .toPromise()
     }
 
     private handleError( error: Response){
-                                if (error.status === 400) 
-                                  return throwError(new BadInput(error.json()))
-                                if (error.status === 404) 
-                                  return throwError(new notFoundError()); 
-                                  return throwError(new appError(error));
+      if (error.status === 400) 
+      return throwError(new BadInput(error.json()))
+      if (error.status === 404) 
+      return throwError(new notFoundError()); 
+      return throwError(new appError(error));
       
     }
 
