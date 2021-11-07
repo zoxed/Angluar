c18740b2c25282ca1c5e2e1a50944e3980d47a65
mohamedkaabi <mohamed.kaabi.cap@gmail.com>
2021-11-07 18:50:53 +0100

    Routes with Multiple parameters

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -11,14 +11,19 @@ export class GithubProfileComponent implements OnInit {
   constructor(private route : ActivatedRoute) { }
 
   ngOnInit() {
+    
     //if you dont want to work an observable do this 
-    // this.route.paramMap['id']
+    // -----> this.route.paramMap['id']
+    
+    //if you are hundred % sure that the client will go to another page and comeback you use snapchat observable 
+    // let id =  this.route.snapshot.paramMap.get('id');
+    // console.log(id)
     this.route.paramMap
     .subscribe(params =>{
       //the get methode returns a string if ou want to switch it to int 
       //you have to add the + signe and store it in a variable
-       let id =  +params.get('id');
-       console.log(id)
+       
+       console.log(params.get('id'));
       })
   }
 
